dad4d1ac8949339f14a289894b8fa7ab8ceff42c
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-06-24 17:41:47 +0100

    created post route

---
+++ api/routes/post.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Post = require("../models/post");
+
+const checkAuth = require("../middleware/check-auth.js");
+
+router.get("/", checkAuth, (req, res) => {
+  Post.find()
+    .select("_id title date author image")
+    .populate({ path: "author", select: "username picture" })
+    .exec()
+    .then((docs) => res.status(200).json(docs))
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+router.get("/:postId", checkAuth, (req, res) => {
+  Post.findById(req.params.postId)
+    .select("_id title date author image")
+    .populate({ path: "author", select: "username picture" })
+    .exec()
+    .then((doc) => res.status(200).json(doc))
+    .catch((err) => res.status(500).json({ err }));
+});
+
+router.post("/", checkAuth, (req, res, next) => {
+  const post = new Post({
+    _id: new mongoose.Types.ObjectId(),
+    title: req.body.title,
+    author: req.body.authorId,
+    image: req.body.image,
+  });
+  post
+    .save()
+    .then((result) => {
+      res.status(200).json({
+        message: "Created the post successfully",
+        postId: result.id,
+      });
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+router.delete("/:postId", checkAuth, (req, res, next) => {
+  Post.remove({ _id: req.params.postId })
+    .exec()
+    .then(() => res.status(200).json({ message: "Post deleted" }))
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+module.exports = router;
