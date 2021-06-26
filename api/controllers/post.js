e989e0369d0d27b83f715e0705c8610ee1b66e06
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-06-26 11:38:14 +0100

    created post controller

---
+++ api/controllers/post.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const User = require("../models/user");
+const Post = require("../models/post");
+
+exports.getPosts = (req, res) => {
+  Post.find()
+    .select("_id title date author image")
+    .populate({ path: "author", select: "username picture" })
+    .exec()
+    .then((docs) => res.status(200).json(docs))
+    .catch((err) => res.status(500).json({ error: err }));
+};
+
+exports.getPost = (req, res) => {
+  Post.findById(req.params.postId)
+    .select("_id title date author image")
+    .populate({ path: "author", select: "username picture" })
+    .exec()
+    .then((doc) => res.status(200).json(doc))
+    .catch((err) => res.status(500).json({ err }));
+};
+
+exports.addPost = (req, res) => {
+  const post = new Post({
+    _id: new mongoose.Types.ObjectId(),
+    title: req.body.title,
+    author: req.body.authorId,
+    image: req.body.image,
+  });
+  post
+    .save()
+    .then((result) => {
+      User.findByIdAndUpdate(req.body.authorId, {
+        $push: { posts: result.id },
+      })
+        .then(() => {
+          res.status(200).json({
+            message: "Created the post successfully",
+            postId: result.id,
+          });
+        })
+        .catch((err) => res.status(404).json({ error: err }));
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+};
+
+exports.deletePost = (req, res) => {
+  Post.remove({ _id: req.params.postId })
+    .exec()
+    .then(() => res.status(200).json({ message: "Post deleted" }))
+    .catch((err) => res.status(500).json({ error: err }));
+};
--- api/routes/post.js
+++ api/routes/post.js
@@ -1,59 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-
-const User = require("../models/user");
-const Post = require("../models/post");
 
 const checkAuth = require("../middleware/check-auth.js");
 
-router.get("/", checkAuth, (req, res) => {
-  Post.find()
-    .select("_id title date author image")
-    .populate({ path: "author", select: "username picture" })
-    .exec()
-    .then((docs) => res.status(200).json(docs))
-    .catch((err) => res.status(500).json({ error: err }));
-});
+const {
+  getPosts,
+  getPost,
+  addPost,
+  deletePost,
+} = require("../controllers/post");
 
-router.get("/:postId", checkAuth, (req, res) => {
-  Post.findById(req.params.postId)
-    .select("_id title date author image")
-    .populate({ path: "author", select: "username picture" })
-    .exec()
-    .then((doc) => res.status(200).json(doc))
-    .catch((err) => res.status(500).json({ err }));
-});
+router.get("/", checkAuth, getPosts);
+router.get("/:postId", checkAuth, getPost);
 
-router.post("/", checkAuth, (req, res, next) => {
-  const post = new Post({
-    _id: new mongoose.Types.ObjectId(),
-    title: req.body.title,
-    author: req.body.authorId,
-    image: req.body.image,
-  });
-  post
-    .save()
-    .then((result) => {
-      User.findByIdAndUpdate(req.body.authorId, {
-        $push: { posts: result.id },
-      })
-        .then(() => {
-          res.status(200).json({
-            message: "Created the post successfully",
-            postId: result.id,
-          });
-        })
-        .catch((err) => res.status(404).json({ error: err }));
-    })
-    .catch((err) => res.status(500).json({ error: err }));
-});
+router.post("/", checkAuth, addPost);
 
-router.delete("/:postId", checkAuth, (req, res, next) => {
-  Post.remove({ _id: req.params.postId })
-    .exec()
-    .then(() => res.status(200).json({ message: "Post deleted" }))
-    .catch((err) => res.status(500).json({ error: err }));
-});
+router.delete("/:postId", checkAuth, deletePost);
 
 module.exports = router;
